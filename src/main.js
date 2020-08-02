0cbdbc0c63f0848b63cfffdafe241da1c5e56db8
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-08-02 19:53:56 +0900

    first commit'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import router from './router'
+import store from './store'
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
