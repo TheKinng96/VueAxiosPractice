c89b8cda2da68528329a1577d9ca5cb77d6159e4
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-08-02 20:30:52 +0900

    axios get and post connected to the data script

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -2,8 +2,38 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Email address: {{email}}</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      email: ''
+    }
+  },
+  created() {
+    axios.get('/users.json')
+    .then(res => {
+      console.log(res)
+      const data = res.data;
+      const users = [];
+      for (let key in data){
+        const user = data[key]
+        user.id = key
+        users.push(user)
+      }
+      console.log(users)
+      this.email = users[0].email
+    })
+    .then
+    .catch(err => console.log(err))
+  }
+}
+</script>
 
 <style scoped>
   h1, p {
