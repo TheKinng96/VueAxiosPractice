1a73b52eb49726c2d4ffaeb992907d9b26a9344d
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-08-03 17:31:24 +0900

    token can be passed in the app

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from './axios-auth'
+import globalAxios from 'axios'
 
 Vue.use(Vuex)
 
@@ -8,16 +9,20 @@ export default new Vuex.Store({
     state: {
         idToken: null,
         userId: null,
+        user: null
     },
     mutations: {
         authUser(state, userData) {
             state.idToken = userData.token;
             state.userId = userData.userId
+        },
+        storeUser(state, user){
+            state.user = user
         }
     },
     actions: {
         // eslint-disable-next-line no-unused-vars
-        signup({commit}, authData) {
+        signup({commit, dispatch}, authData) {
             axios.post('/accounts:signUp?key=AIzaSyBziIMIOQ2yPdKXNb-wG6cfMoco4w8Eqjg', {
                 email: authData.email,
                 password: authData.password,
@@ -29,10 +34,10 @@ export default new Vuex.Store({
                         token: response.data.idToken,
                         userId: response.data.localId
                     })
+                    dispatch('storeUser', authData)
                 })
                 .catch(error => console.log(error))
         },
-        // eslint-disable-next-line no-unused-vars
         signin({commit}, authData) {
             axios.post('/accounts:signInWithPassword?key=AIzaSyBziIMIOQ2yPdKXNb-wG6cfMoco4w8Eqjg', {
                 email: authData.email,
@@ -46,7 +51,36 @@ export default new Vuex.Store({
                 })
             })
                 .catch(error => console.log(error))
+        },
+        // eslint-disable-next-line no-unused-vars
+        storeUser ({commit, state}, userData){
+            if (!state.idToken){
+                return
+            }
+            globalAxios.post('/users.json' + '?auth=' + state.idToken, userData)
+                .then(res => console.log(res))
+                .catch(err => console.log(err))
+        },
+        fetchData({commit, state}){
+            if (!state.idToken){
+                return
+            }
+            globalAxios.get('/users.json' + '?auth=' + state.idToken)
+                .then(res => {
+                    const data = res.data;
+                    const users = []
+                    for (let key in data) {
+                        const user = data[key]
+                        user.id = key;
+                        users.push(user)
+                    }
+                    commit('storeUser', users[0])
+                }).catch(err => console.log(err))
         }
     },
-    getters: {}
+    getters: {
+        user(state){
+            return state.user
+        }
+    }
 })
