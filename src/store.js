f7ca8a368ec290280878939860aea31577cb46ae
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-08-03 16:18:27 +0900

    finished moving the signup and login codes into the vuex store

--- src/store.js
+++ src/store.js
@@ -5,33 +5,48 @@ import axios from './axios-auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    idToken: null,
-    userId: null,
-  },
-  mutations: {
-
-  },
-  actions: {
-    signup({commit}, authData){
-      axios.post('/accounts:signUp?key=AIzaSyBziIMIOQ2yPdKXNb-wG6cfMoco4w8Eqjg', {
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      })
-          .then(response => console.log(response))
-          .catch(error => console.log(error))
+    state: {
+        idToken: null,
+        userId: null,
     },
-    signin({commit},authData){
-      axios.post('/accounts:signInWithPassword?key=AIzaSyBziIMIOQ2yPdKXNb-wG6cfMoco4w8Eqjg', {
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      }).then(response => console.log(response))
-          .catch(error => console.log(error))
-    }
-  },
-  getters: {
-
-  }
+    mutations: {
+        authUser(state, userData) {
+            state.idToken = userData.token;
+            state.userId = userData.userId
+        }
+    },
+    actions: {
+        // eslint-disable-next-line no-unused-vars
+        signup({commit}, authData) {
+            axios.post('/accounts:signUp?key=AIzaSyBziIMIOQ2yPdKXNb-wG6cfMoco4w8Eqjg', {
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            })
+                .then(response => {
+                    console.log(response)
+                    commit('authUser', {
+                        token: response.data.idToken,
+                        userId: response.data.localId
+                    })
+                })
+                .catch(error => console.log(error))
+        },
+        // eslint-disable-next-line no-unused-vars
+        signin({commit}, authData) {
+            axios.post('/accounts:signInWithPassword?key=AIzaSyBziIMIOQ2yPdKXNb-wG6cfMoco4w8Eqjg', {
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }).then(response => {
+                console.log(response)
+                commit('authUser', {
+                    token: response.data.idToken,
+                    userId: response.data.localId
+                })
+            })
+                .catch(error => console.log(error))
+        }
+    },
+    getters: {}
 })
